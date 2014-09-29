a771ffd8295190f08a8ea5b218d1e8a4868b39b2
Alexey Preobrazhensky <dizzy57@gmail.com>
2014-09-30 01:36:44 +0400

    Use initializer lists for pairs.

--- arrays/intersection.cpp
+++ arrays/intersection.cpp
@@ -36,8 +36,8 @@ pair<int,int> maxIntersection(vector<pair<int,int>>& v) {
     for (int i = 0; i < v.size(); i++) {
         if (v[i].first > v[i].second)
             throw "bad input";
-        points.insert(make_pair(v[i].first, true));
-        points.insert(make_pair(v[i].second, false));
+        points.insert({v[i].first, true});
+        points.insert({v[i].second, false});
     }
     
     int maxInt = 0, maxPoint = 0;
@@ -54,7 +54,7 @@ pair<int,int> maxIntersection(vector<pair<int,int>>& v) {
         }
     }
     
-    return make_pair(maxInt, maxPoint);
+    return {maxInt, maxPoint};
 }
 
 int main(int argc, const char * argv[])
@@ -62,35 +62,35 @@ int main(int argc, const char * argv[])
     try {
     
         // 1 test
-        vector<pair<int,int>> v {make_pair(1,3), make_pair(2,4)};
+        vector<pair<int,int>> v {{1,3}, {2,4}};
         pair<int,int> res = maxIntersection(v);
         if (res.first != 2 || res.second != 2)
             throw "First test failed";
         
         // 2 test
         v.clear();
-        v = {make_pair(5,7), make_pair(3,4)};
+        v = {{5,7}, {3,4}};
         res = maxIntersection(v);
         if (res.first != 1 || res.second != 3)
             throw "Second test failed";
         
         // 3 test
         v.clear();
-        v = {make_pair(-1,-1), make_pair(-3,-3), make_pair(100, 1000), make_pair(50, 120), make_pair(0,0)};
+        v = {{-1,-1}, {-3,-3}, {100, 1000}, {50, 120}, {0,0}};
         res = maxIntersection(v);
         if (res.first != 2 || res.second != 100)
             throw "Third test failed";
 
         // 4 test
         v.clear();
-        v = {make_pair(-1,-1), make_pair(-1,-1)};
+        v = {{-1,-1}, {-1,-1}};
         res = maxIntersection(v);
         if (res.first != 2 || res.second != -1)
             throw "Fourth test failed";
 
         // 5 test
         v.clear();
-        v = {make_pair(-2,-1), make_pair(-1,0)};
+        v = {{-2,-1}, {-1,0}};
         res = maxIntersection(v);
         if (res.first != 2 || res.second != -1)
             throw "Fifth test failed";
