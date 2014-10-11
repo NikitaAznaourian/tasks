77b2066d14c7dd81dbe50595f1af5f1dbb98a01d
NikitaAznaourian <nikita-aznaourian@yandex.ru>
2014-10-11 13:12:12 +0400

    rledecoding

---
+++ strings/rledecoding.cpp
@@ -0,0 +1,156 @@
+//
+//  main.cpp
+//  tmp
+//
+//  Created by Nikita Aznauryan on 10.07.14.
+//  Copyright (c) 2014 Nikita Aznauryan. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <set>
+#include <map>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+/* Condition:
+    RLE encoding - encode the string
+ 
+   Solution:
+    Complexity: O(n) - пройтись по строке
+    Additional memory: O(1)
+*/
+
+bool isDigit(const char& c) {
+    if ((int)'0' <= (int) c && (int) c <= (int) '9')
+        return true;
+    return false;
+}
+
+enum State {Init, Digit, Char};
+
+void RLEDecoding(const string& s, string& res) {
+    State st = Init;
+    size_t pos = 0;
+    string digit;
+    
+    while (pos < s.size()) {
+        if (st == Init) {
+            if (isDigit(s[pos])) {
+                digit.append(1,s[pos]);
+                st = Digit;
+            } else {
+                throw "Not a digit at the begining";
+            }
+        } else if (st == Digit) {
+            if (isDigit(s[pos])) {
+                digit.append(1,s[pos]);
+            } else {
+                res.append(atoi(digit.c_str()), s[pos]);
+                st = Char;
+                digit.clear();
+            }
+        } else if (st == Char) {
+            if (isDigit(s[pos])) {
+                digit.append(1,s[pos]);
+                st = Digit;
+            } else {
+                throw "Letter after letter";
+            }
+        }
+        pos++;
+    }
+    
+    if (st == Digit)
+        throw "Digit without a letter at the end";
+    return;
+}
+
+
+int main(int argc, const char * argv[])
+{
+    try {
+        
+        bool isExeption = false;
+        string s = "";
+        string res;
+
+        // 1 test
+        RLEDecoding(s, res);
+        if (res != "")
+            throw "1 test failed";
+        
+        // 2 test
+        s = "a1";
+        res.clear();
+        isExeption = false;
+
+        try {
+            RLEDecoding(s, res);
+        } catch(char const* err) {
+            isExeption = true;
+        }
+
+        if (!isExeption)
+            throw "2 test failed";
+
+        // 3 test
+        s = "2a3";
+        res.clear();
+        isExeption = false;
+        
+        try {
+            RLEDecoding(s, res);
+        } catch(char const* err) {
+            isExeption = true;
+        }
+        
+        if (!isExeption)
+            throw "3 test failed";
+
+        // 4 test
+        s = "2a3ss";
+        res.clear();
+        isExeption = false;
+        
+        try {
+            RLEDecoding(s, res);
+        } catch(char const* err) {
+            isExeption = true;
+        }
+        
+        if (!isExeption)
+            throw "4 test failed";
+
+        // 5 test
+        s = "2a3s1d";
+        res.clear();
+        isExeption = false;
+
+        RLEDecoding(s, res);
+        if (res != "aasssd")
+            throw "5 test failed";
+
+        // 6 test
+        s = "12a1d";
+        res.clear();
+        isExeption = false;
+        
+        RLEDecoding(s, res);
+        if (res != "aaaaaaaaaaaad")
+            throw "6 test failed";
+
+        cout << "All tests successed" << endl;
+ 
+    } catch (char const* err) {
+        cout << err << endl;
+    }
+
+    return 0;
+}
+
