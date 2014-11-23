c5cd847adddbfe919613ffa107c89ae84d150329
NikitaAznaourian <nikita-aznaourian@yandex.ru>
2014-11-23 15:22:28 +0300

    add insert operation for bst

---
+++ mytasks/trees/bstOperations.cpp
@@ -0,0 +1,37 @@
+class Node {
+public:
+    Node(int val) : id(val) {};
+    int id;
+    Node* left = nullptr, *right = nullptr, *parent = nullptr;
+};
+
+void treeInsert(Node* root, Node* z) {
+    if (!root) {
+        root = z;
+        return;
+    }
+    
+    if (z->id >= root->id) {
+        if (root->right) {
+            treeInsert(root->right, z);
+        } else {
+            root->right = z;
+            z->parent = root;
+        }
+    } else {
+        if (root->left) {
+            treeInsert(root->left, z);
+        } else {
+            root->left = z;
+            z->parent = root;
+        }
+    }
+    
+    return;
+}
+
+
+int main() {
+
+    return 0;
+};
